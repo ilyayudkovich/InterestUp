8d6a667131bb99739b5e08d244d415215455152a
Mackenzie Denker <mackenziedenker@gmail.com>
2018-02-28 13:24:29 -0500

    Add profile page and way to get there.

--- app/src/main/java/com/example/mdenker/interestup/Home.java
+++ app/src/main/java/com/example/mdenker/interestup/Home.java
@@ -1,7 +1,10 @@
 package com.example.mdenker.interestup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //add back button
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    public void OnProfileClick(View view) {
+        Intent i = new Intent(this, Profile.class);
+        startActivity(i);
+    }
+
 }
