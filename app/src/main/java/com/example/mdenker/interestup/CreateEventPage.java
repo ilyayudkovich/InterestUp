14af22e90dc130006bc052d5decd2e1092ca0683
Jorge Delgado <rosefox911@gmail.com>
2018-02-28 14:44:08 -0500

    Removing floating button from create event page Fleshed out "general" in create event page Currently there is a small issue where everything in "General" is being mirrored to "Advanced"

--- app/src/main/java/com/example/mdenker/interestup/CreateEventPage.java
+++ app/src/main/java/com/example/mdenker/interestup/CreateEventPage.java
@@ -55,16 +55,6 @@ public class CreateEventPage extends AppCompatActivity {
 
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
     }
 
 
@@ -118,9 +108,9 @@ public class CreateEventPage extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_create_event_page, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            View rootView = inflater.inflate(R.layout.create_event_general_tab, container, false);
+            //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
     }
