7237d13660cb049e35347619329cb32ff8190045
Jorge Delgado <rosefox911@gmail.com>
2018-02-28 15:07:51 -0500

    Fleshing out create event tabs further. Adding Java classes for two tabs.

--- app/src/main/java/com/example/mdenker/interestup/CreateEventPage.java
+++ app/src/main/java/com/example/mdenker/interestup/CreateEventPage.java
@@ -80,40 +80,7 @@ public class CreateEventPage extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.create_event_general_tab, container, false);
-            //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
+    //Deleted PlaceholderFragment class from here since each tab has it's own class now.
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -127,15 +94,34 @@ public class CreateEventPage extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            //Returning the current tab
+            switch(position) {
+                case 0:
+                    CreateEventPage_GeneralTab GeneralTab = new CreateEventPage_GeneralTab();
+                    return GeneralTab;
+                case 1:
+                    CreateEventPage_AdvancedTab AdvancedTab = new CreateEventPage_AdvancedTab();
+                    return AdvancedTab;
+                default:
+                    return null;
+            }
         }
 
         @Override
         public int getCount() {
             // Show 3 total pages.
             return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch(position) {
+                case 0:
+                    return "GENERAL";
+                case 1:
+                    return "ADVANCED";
+            }
+            return null;
         }
     }
 }
