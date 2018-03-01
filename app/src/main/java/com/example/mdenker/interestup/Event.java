2c0e942344980eb2fe308220b47c3a6ea59413c2
Jorge Delgado <rosefox911@gmail.com>
2018-02-28 22:21:49 -0500

    Expanded Event class to include all fields needed.

--- app/src/main/java/com/example/mdenker/interestup/Event.java
+++ app/src/main/java/com/example/mdenker/interestup/Event.java
@@ -1,6 +1,10 @@
 package com.example.mdenker.interestup;
 
+import android.text.format.DateFormat;
+import android.text.format.Time;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
 /**
@@ -8,25 +12,55 @@ import java.util.GregorianCalendar;
  */
 
 public class Event {
+    int id;
     String name;
-    String date;
+    String description;
+    DateFormat startDate;
+    DateFormat endDate;
+    Time startTime;
+    Time endTime;
+    boolean tentativeDates;
     String location;
+    ArrayList tags;
+    int numberOfAttendees;
+    String viewRestrictions;
+    ArrayList exclusions;
+
 
     Event(String name, String date, String location) {
+        this.id = id;
         this.name = name;
-        this.date = date;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.tentativeDates = tentativeDates;
         this.location = location;
+        this.tags = tags;
+        this.numberOfAttendees = numberOfAttendees;
+        this.viewRestrictions = viewRestrictions;
+        this.exclusions = exclusions;
     }
-
+    public int getID() {
+        return this.id;
+    }
     public String getName() {
         return this.name;
     }
-
-    public String getDate() {
-        return this.date;
+    public String getDescription() {
+        return this.description;
     }
-
-    public String getLocation() {
-        return this.location;
+    public DateFormat getStartDate() {
+        return this.startDate;
     }
+    public DateFormat getEndDate() {return this.endDate;}
+    public Time getStartTime() {return this.startTime;}
+    public Time getEndTime() {return this.endTime;}
+    public boolean getTentativeDates() {return this.tentativeDates;}
+    public String getLocation() {return this.location;}
+    public ArrayList getTags() {return this.tags;}
+    public int getNumberOfAttendees() { return this.numberOfAttendees;}
+    public String getViewRestrictions() {return this.viewRestrictions;}
+    public ArrayList getExclusions() {return this.exclusions;}
 }
