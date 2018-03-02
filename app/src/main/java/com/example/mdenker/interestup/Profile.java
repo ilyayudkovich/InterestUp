1401741e9a108920115ae897c4fc657a8ed3e796
Mackenzie Denker <mackenziedenker@gmail.com>
2018-03-01 21:13:09 -0500

    Profile page changes.

--- app/src/main/java/com/example/mdenker/interestup/Profile.java
+++ app/src/main/java/com/example/mdenker/interestup/Profile.java
@@ -1,14 +1,31 @@
 package com.example.mdenker.interestup;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.MenuItem;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 
 /**
@@ -23,6 +40,12 @@ public class Profile extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+    private boolean enabled = true;
+
+    ImageButton backButton;
+    ImageButton editButton;
+    Button cancelButton;
+    Button doneButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,15 +63,110 @@ public class Profile extends AppCompatActivity {
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
+        backButton = (ImageButton) findViewById(R.id.backButton);
+        editButton = (ImageButton) findViewById(R.id.editButton);
+        cancelButton = (Button) findViewById(R.id.cancelButton);
+        doneButton = (Button) findViewById(R.id.doneButton);
+
+ /*
+        int index = 0;
+        interestsLayout = findViewById(R.id.InterestContainer);
+        LinearLayout interestRow = new LinearLayout(this);
+
+        interestsLayout.addView(interestRow);
+
+        interestRow.setOrientation(LinearLayout.HORIZONTAL);
+        interestRow.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        topInterestRow = interestRow;
+
+        while (index < interests.size()) {
+            if (index > 0 && index % 2 == 0) {
+                interestRow = new LinearLayout(this);
+                interestsLayout.addView(interestRow);
+                interestRow.setOrientation(LinearLayout.HORIZONTAL);
+                interestRow.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                topInterestRow = interestRow;
+            }
+            TextView interestItem = new TextView(this);
+            interestItem.setText(interests.get(index));
+            interestRow.addView(interestItem);
+            //ViewGroup.LayoutParams lparams = new ViewGroup.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            //titleView.setLayoutParams(lparams);
+            //interestItem.setTextAppearance(this, android.R.attr.textAppearanceLarge); // TODO how to add text appearance in xml
+            index++;
+        }
+*/
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        OnEditClick(mViewPager);
     }
 
     public void OnBackClick(View view) {
         this.finish();
     }
 
+
+
+    private void ForceExitKeyboard(View view) {
+        //View view = this.getCurrentFocus();
+        //if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+       // }
+  //      view.restoreDefaultFocus();
+    }
+
+    private void EnableDisableView(View view) {
+
+        if (view.getId() != R.id.AddInterest) {
+
+            if (!enabled && view.isFocused()) {
+                ForceExitKeyboard(view);
+                view.clearFocus(); // makes sure you're not still focused no the item after removing keyboard
+            }
+
+            view.setFocusableInTouchMode(enabled);
+
+
+            if (view instanceof ViewGroup) {
+                ViewGroup group = (ViewGroup) view;
+
+                for (int idx = 0; idx < group.getChildCount(); idx++) {
+                    EnableDisableView(group.getChildAt(idx));
+                }
+            }
+        }
+    }
+
+    public void OnScreenTapped(View view) {
+        ForceExitKeyboard(view);
+    }
+
+
     public void OnEditClick(View view) {
-        //Intent i = new Intent(this, Profile.class); // figure out this
-        //startActivity(i);
+        //mViewPager.setFocusable(true);
+        enabled = !enabled;
+
+        if (!enabled) {
+        //    ForceExitKeyboard();
+            doneButton.setVisibility(View.GONE);
+            cancelButton.setVisibility(View.GONE);
+
+            editButton.setVisibility(View.VISIBLE);
+            backButton.setVisibility(View.VISIBLE);
+        }
+        else {
+            doneButton.setVisibility(View.VISIBLE);
+            cancelButton.setVisibility(View.VISIBLE);
+
+            editButton.setVisibility(View.GONE);
+            backButton.setVisibility(View.GONE);
+        }
+        EnableDisableView(mViewPager);
     }
 
     public class ProfileTabAdapter extends SectionsPagerAdapter {
@@ -65,7 +183,6 @@ public class Profile extends AppCompatActivity {
                     Profile_InterestsTab firstTab = new Profile_InterestsTab();
                     return firstTab;
                 case 1:
-                    System.out.println("hi there");
                     Profile_PersonalInfoTab secondTab = new Profile_PersonalInfoTab();
                     return secondTab;
                 default:
--- app/src/main/java/com/example/mdenker/interestup/Profile_InterestsTab.java
+++ app/src/main/java/com/example/mdenker/interestup/Profile_InterestsTab.java
@@ -1,10 +1,23 @@
 package com.example.mdenker.interestup;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Created by Jorge Delgado on 2/28/2018.
@@ -12,9 +25,130 @@ import android.view.ViewGroup;
 
 public class Profile_InterestsTab extends Fragment {
 
+    private ArrayList<String> interests = new ArrayList<String>();
+    //private String[] interests = {"Music Performance", "Hiking", "Improv", "Writing",
+    //        "Frisbee", "Twilight"};
+
+    ArrayAdapter<String> interestArrayAdapter;
+    ArrayAdapter<String> editInterestArrayAdapter;
+
+    AutoCompleteTextView addInterest;
+    GridView gridView;
+    GridView gridViewEdit;
+
+    TextView plusButton;
+
+    ImageButton editButton;
+    Button cancelButton;
+    Button doneButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.profile_interests_tab, container, false);
+
+        plusButton = (TextView) rootView.findViewById(R.id.plusButton1);
+
         return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+        // ListView listView = (ListView) findViewById(R.id.lvItems);
+        //listView.setAdapter(itemsAdapter);
+
+        Collections.addAll(interests,"Music Performance", "Hiking", "Improv", "Writing",
+             "Frisbee", "Twilight");
+
+        // have two grids, one gone, with edit version
+
+        interestArrayAdapter = new ArrayAdapter<String>(getActivity(), R.layout.interest_item, interests);
+        editInterestArrayAdapter = new ArrayAdapter<String>(getActivity(), R.layout.interest_item_edit, interests);
+
+        gridView = (GridView) view.findViewById(R.id.gridView1);
+        gridView.setAdapter(interestArrayAdapter);
+
+        gridViewEdit = (GridView) view.findViewById(R.id.gridView1Edit);
+        gridViewEdit.setAdapter(editInterestArrayAdapter);
+
+        gridViewEdit.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+                // TODO Auto-generated method stub
+                parent.removeViewAt(position);
+            }
+        });
+/*
+        editButton = (ImageButton) getActivity().findViewById(R.id.editButton);
+        cancelButton = (Button) getActivity().findViewById(R.id.cancelButton);
+        doneButton = (Button) getActivity().findViewById(R.id.doneButton);
+
+        editButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                gridViewEdit.setVisibility(View.VISIBLE);
+                gridView.setVisibility(View.GONE);
+            }
+        });
+        cancelButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                gridViewEdit.setVisibility(View.GONE);
+                gridView.setVisibility(View.VISIBLE);
+            }
+        });
+        doneButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                gridViewEdit.setVisibility(View.GONE);
+                gridView.setVisibility(View.VISIBLE);
+            }
+        });
+
+*/
+        addInterest = view.findViewById(R.id.AddInterest);
+
+
+        plusButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (addInterest.getText().length() > 3) {
+                    interestArrayAdapter.add(addInterest.getText().toString());
+                    addInterest.setText("");
+                }
+            }
+        });
+
+        addInterest.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() > 2) { // at least 0, but 2 for real words
+                    if (s.charAt(s.length() - 1) == '\n') {
+                        interestArrayAdapter.add(s.subSequence(0, s.length() - 1).toString());
+                        addInterest.setText("");
+                    }
+                }
+            }
+        });
     }
 }
